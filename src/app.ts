38b6c90077e6caeffa8f68cec57b3eff6f206c09
enzovalencuela <esilvavalencuela@gmail.com>
2025-05-03 17:46:21 -0400

    Adicionando logs no app pra ver oq nos mostra pra tentar resolver o erro do cors que persiste

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,15 @@ import cors from "cors";
 
 const app = express();
 
+console.log("Configurando CORS..."); // Adicione este log
 app.use(
   cors({
     origin: "https://megajr-front-end.vercel.app",
   })
 );
+console.log("CORS configurado."); // Adicione este log
 app.use(express.json());
 app.use("/", userRoutes);
+console.log("Rotas de usuário adicionadas."); // Adicione este log
 
 export default app;
