e4274e4b2977e7f79c5f6058a2225aac8e423c67
enzovalencuela <esilvavalencuela@gmail.com>
2025-05-04 11:12:29 -0400

    Adicionando confg do novo dominio do front

--- src/app.ts
+++ src/app.ts
@@ -10,14 +10,7 @@ app.use((req, res, next) => {
 });
 
 console.log("Configurando CORS..."); // Adicione este log
-app.use(
-  cors({
-    origin: "https://megajr-front-end.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+app.use(cors());
 console.log("CORS configurado."); // Adicione este log
 app.use(express.json());
 app.use("/", userRoutes);
