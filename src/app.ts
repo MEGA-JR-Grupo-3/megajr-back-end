56987c8722e3a5d4395e154f430ebefa52d27f57
enzovalencuela <esilvavalencuela@gmail.com>
2025-05-04 11:59:19 -0400

    Ajustando app

--- src/app.ts
+++ src/app.ts
@@ -4,17 +4,17 @@ import cors from "cors";
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log(`Recebida requisição: ${req.method} ${req.url}`);
-  next();
-});
-
-console.log("Configurando CORS..."); // Adicione este log
 app.use(
   cors({
     origin: "https://megajr-front.netlify.app",
   })
 );
+
+app.use((req, res, next) => {
+  console.log(`Recebida requisição: ${req.method} ${req.url}`);
+  next();
+});
+
 console.log("CORS configurado."); // Adicione este log
 app.use(express.json());
 app.use("/", userRoutes);
