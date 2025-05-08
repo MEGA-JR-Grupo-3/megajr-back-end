28f6f7c200417a19105b7d8169222552b954f774
enzovalencuela <esilvavalencuela@gmail.com>
2025-05-07 22:29:53 -0400

    Removendo o task.routes.ts e deixando tudo no arquivo user.routes.ts

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -7,6 +7,12 @@ import {
   getUserData,
 } from "../controllers/user.controller.js";
 
+import {
+  searchTasks,
+  getTasksByUser,
+  addTask,
+} from "../controllers/task.controller.js";
+
 const router = express.Router();
 
 // Rota para verificar se o usuário existe com base no email
@@ -25,3 +31,14 @@ router.post("/cadastro-google", handleGoogleLogin);
 router.post("/user-data", getUserData);
 
 export default router;
+
+//ROTAS DAS TAREFAS --------------------------------------------------------------------------------------------
+
+// Rota para buscar tarefas com filtro por título
+router.get("/tasks/search", searchTasks);
+
+// Rota para pegar todas as tarefas de um usuário (existente)
+router.post("/tasks", getTasksByUser);
+
+// Rota para adicionar uma nova tarefa
+router.post("/tasks/add", addTask);
